c9f54124a7a45d116769b8eaa0496780dcd7e89b
mattia-barbaresi <mat@DESKTOP-MAT>
2016-12-22 19:37:36 +0100

    vector stream updated

--- src/main/java/me/mneri/ca/tests/Test.java
+++ src/main/java/me/mneri/ca/tests/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		int[] stream = new int[] { 1, 1, 1, 1, 1, 1 };
+		int[] stream = new int[] { 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1 };
 		int[] stream2 = new int[] { 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1 };
 
 		// Lizier Entropy calculator
@@ -19,19 +19,19 @@ public class Test {
 		// Our Local entropy
 		System.out.printf("(Our) Local Entropy (of 1s): %f \n", Entropy.localBinaryEntropy(stream, 1));
 		System.out.printf("(Our) Local Entropy (of 0s): %f \n", Entropy.localBinaryEntropy(stream, 0));
-		
-		//Our global joint entropies
+
+		// Our global joint entropies
 		System.out.printf("(Our) Global Joint Entropy: %f \n", Entropy.globalJointEntropy(stream, stream2));
-				
-		//Our local joint entropies
+
+		// Our local joint entropies
 		System.out.printf("(Our) Local Joint Entropy (foreach pair): \n");
 		double res[][] = Entropy.localJointEntropy(stream, stream2);
 		System.out.printf("	-(0,0): %f \n", res[0][0]);
 		System.out.printf("	-(0,1): %f \n", res[0][1]);
 		System.out.printf("	-(1,0): %f \n", res[1][0]);
 		System.out.printf("	-(1,1): %f \n", res[1][1]);
-		
-		//Our global joint entropy
+
+		// Our global joint entropy
 
 		// Lizier local entropy
 		System.out.printf("Lizier Local Entropy (foreach cell): \n");
