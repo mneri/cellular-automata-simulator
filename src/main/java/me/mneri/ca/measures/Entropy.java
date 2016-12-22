b9d53c6c17be2de44187a34387564092d19f2efc
Massimo Neri <hello@mneri.me>
2016-12-22 20:02:31 +0100

    Indentation

--- src/main/java/me/mneri/ca/measures/Entropy.java
+++ src/main/java/me/mneri/ca/measures/Entropy.java
@@ -4,86 +4,86 @@ import me.mneri.ca.util.MathUtils;
 
 public class Entropy {
 
-	// calculate global entropy in a binary data stream
-	public static double globalBinaryEntropy(int[] states) {
+    // calculate global entropy in a binary data stream
+    public static double globalBinaryEntropy(int[] states) {
 
-		float ones = 0;
-		for (int i = 0; i < states.length; i++)
-			ones += states[i];
+        float ones = 0;
+        for (int i = 0; i < states.length; i++)
+            ones += states[i];
 
-		double fone = ones / states.length;
-		double fzero = 1 - fone;
-		if (fone == 0.0 || fzero == 0.0)
-			return 0;
-		
-		return -(fone * MathUtils.log2(fone) + fzero * MathUtils.log2(fzero));
-	}
+        double fone = ones / states.length;
+        double fzero = 1 - fone;
+        if (fone == 0.0 || fzero == 0.0)
+            return 0;
 
-	// calculate local entropy in a binary data stream for given value
-	public static double localBinaryEntropy(int[] states, int value) {
+        return -(fone * MathUtils.log2(fone) + fzero * MathUtils.log2(fzero));
+    }
 
-		if (value == 0 || value == 1) {
+    // calculate global joint entropy given two data streams
+    public static double globalJointEntropy(int[] x, int[] y) {
 
-			float mVal = 0;
-			for (int i = 0; i < states.length; i++) {
-				if (states[i] == value)
-					mVal++;
-			}
-			double fval = mVal / states.length;
+        double[][] freqs = pairsFrequencies(x, y);
+        double[][] ljes = localJointEntropy(x, y);
+        double res = 0.0;
 
-			return -MathUtils.log2(fval);
+        // averaged sum over local joint entropies
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                res += (ljes[i][j] * freqs[i][j]);
 
-		} else
-			throw new IllegalArgumentException("Value must be 1 or 0");
-	}
+        return res;
+    }
 
-	// calculate local joint entropy given two data streams
-	public static double[][] localJointEntropy(int[] x, int[] y) {
+    // calculate local entropy in a binary data stream for given value
+    public static double localBinaryEntropy(int[] states, int value) {
 
-		double[][] result = pairsFrequencies(x, y);
+        if (value == 0 || value == 1) {
 
-		// computing local joint entropies
-		result[0][0] = -MathUtils.log2(result[0][0]);
-		result[0][1] = -MathUtils.log2(result[0][1]);
-		result[1][0] = -MathUtils.log2(result[1][0]);
-		result[1][1] = -MathUtils.log2(result[1][1]);
+            float mVal = 0;
+            for (int i = 0; i < states.length; i++) {
+                if (states[i] == value)
+                    mVal++;
+            }
+            double fval = mVal / states.length;
 
-		return result;
-	}
+            return -MathUtils.log2(fval);
 
-	// calculate global joint entropy given two data streams
-	public static double globalJointEntropy(int[] x, int[] y) {
+        } else
+            throw new IllegalArgumentException("Value must be 1 or 0");
+    }
 
-		double[][] freqs = pairsFrequencies(x, y);
-		double[][] ljes = localJointEntropy(x, y);
-		double res = 0.0;
+    // calculate local joint entropy given two data streams
+    public static double[][] localJointEntropy(int[] x, int[] y) {
 
-		// averaged sum over local joint entropies
-		for (int i = 0; i < 2; i++)
-			for (int j = 0; j < 2; j++)
-				res += (ljes[i][j] * freqs[i][j]);
+        double[][] result = pairsFrequencies(x, y);
 
-		return res;
-	}
+        // computing local joint entropies
+        result[0][0] = -MathUtils.log2(result[0][0]);
+        result[0][1] = -MathUtils.log2(result[0][1]);
+        result[1][0] = -MathUtils.log2(result[1][0]);
+        result[1][1] = -MathUtils.log2(result[1][1]);
 
-	// calculate relative frequence (for each pair) given two data streams
-	private static double[][] pairsFrequencies(int[] x, int[] y) {
+        return result;
+    }
 
-		double[][] result = new double[2][2];
-		int n = x.length;
+    // calculate relative frequence (for each pair) given two data streams
+    private static double[][] pairsFrequencies(int[] x, int[] y) {
 
-		// count pairs occurrences: (0,0) , (0,1) , (1,0) , (1,1)
-		for (int i = 0; i < n; i++) {
-			result[x[i]][y[i]]++;
-		}
+        double[][] result = new double[2][2];
+        int n = x.length;
 
-		// computing frequencies
-		result[0][0] /= n;
-		result[0][1] /= n;
-		result[1][0] /= n;
-		result[1][1] /= n;
+        // count pairs occurrences: (0,0) , (0,1) , (1,0) , (1,1)
+        for (int i = 0; i < n; i++) {
+            result[x[i]][y[i]]++;
+        }
 
-		return result;
-	}
+        // computing frequencies
+        result[0][0] /= n;
+        result[0][1] /= n;
+        result[1][0] /= n;
+        result[1][1] /= n;
+
+        return result;
+    }
 
 }
