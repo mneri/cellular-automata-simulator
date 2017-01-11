d571349b483354ddfe43475ef20c62a14247fe28
Massimo Neri <hello@mneri.me>
2017-01-11 09:50:00 +0100

    Cleaning

--- src/test/java/me/mneri/ca/test/Test.java
+++ src/test/java/me/mneri/ca/test/Test.java
@@ -1,12 +1,8 @@
 package me.mneri.ca.test;
 
-import static org.junit.Assert.*;
-
 public class Test {
-
     @org.junit.Test
     public void entropy() {
-        //fail("Not yet implemented");
-    }
 
+    }
 }
