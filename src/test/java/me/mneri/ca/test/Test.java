47f70b32e99c921d86c224a1296ab9e80f34cfbf
mattia-barbaresi <mat@DESKTOP-MAT>
2017-01-11 12:55:15 +0100

    entropy rate wrongly implemented + tests

--- src/test/java/me/mneri/ca/test/Test.java
+++ src/test/java/me/mneri/ca/test/Test.java
@@ -4,8 +4,17 @@ import static org.junit.Assert.*;
 
 public class Test {
 
+    //generazione random input streams
+    
     @org.junit.Test
-    public void entropy() {
+    public void localEntropy() {
+        //assertArrayEquals(expecteds, actuals);
+        //fail("Not yet implemented");
+    }
+    
+    @org.junit.Test
+    public void globalEntropy() {
+        //assertArrayEquals(expecteds, actuals);
         //fail("Not yet implemented");
     }
 
