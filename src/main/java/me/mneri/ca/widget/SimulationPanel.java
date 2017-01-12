b0012006cdbe69d91aabd0768b2a828c9f6aeb3e
Massimo Neri <hello@mneri.me>
2017-01-12 17:21:52 +0100

    Fixed zoom in SimulationPanel

--- src/main/java/me/mneri/ca/widget/SimulationPanel.java
+++ src/main/java/me/mneri/ca/widget/SimulationPanel.java
@@ -1,41 +1,44 @@
 package me.mneri.ca.widget;
 
-import static java.awt.RenderingHints.*;
+import me.mneri.ca.drawable.SpaceTimeDiagram;
 
+import javax.swing.*;
 import java.awt.*;
 import java.util.Set;
 
-import javax.swing.*;
-
-import me.mneri.ca.drawable.SpaceTimeDiagram;
+import static java.awt.RenderingHints.KEY_TEXT_ANTIALIASING;
+import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
 
 public class SimulationPanel extends JPanel {
     private static final Color CELL_COLOR = Color.decode("#cdcdcd");
-    private static final int CELL_SIZE = 4;
     private static final RenderingHints HINTS = new RenderingHints(KEY_TEXT_ANTIALIASING, VALUE_TEXT_ANTIALIAS_ON);
     private static final double SCALE_FACTOR = 1.2d;
+    private static final double SCALE_DEFAULT = 4.0d;
+    private static final double SCALE_MIN = 1.0d;
+    private static final double SCALE_MAX = 8.0d;
 
     private Color mBackgroundColor;
     private SpaceTimeDiagram mDiagram;
-    private int mScrollX;
-    private int mScrollY;
-    private double mScale = 1.0d;
+    private double mScrollX;
+    private double mScrollY;
+    private double mScale = SCALE_DEFAULT;
     private double mZoomFactor = SCALE_FACTOR;
 
     @Override
     protected void paintComponent(Graphics graphics) {
         Graphics2D g = (Graphics2D) graphics;
+        g.scale(mScale, mScale);
         g.setRenderingHints(HINTS);
 
         int width = getWidth();
         int height = getHeight();
 
-        int gridLeft = mScrollX / CELL_SIZE;
-        int gridWidth = width / CELL_SIZE + 1;
+        int gridLeft = (int) mScrollX;
+        int gridWidth = (int) Math.ceil(width / mScale) + 1;
         int gridRight = gridLeft + gridWidth;
 
-        int gridTop = mScrollY / CELL_SIZE;
-        int gridHeight = height / CELL_SIZE + 1;
+        int gridTop = (int) mScrollY;
+        int gridHeight = (int) Math.ceil(height / mScale) + 1;
         int gridBottom = gridTop + gridHeight;
 
         g.setColor(mBackgroundColor);
@@ -48,18 +51,18 @@ public class SimulationPanel extends JPanel {
 
             for (int j = gridLeft; j < gridRight; j++) {
                 if (state.contains(j)) {
-                    int x = j * CELL_SIZE - mScrollX;
-                    int y = i * CELL_SIZE - mScrollY;
+                    int x = (int) (j - mScrollX);
+                    int y = (int) (i - mScrollY);
 
-                    g.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+                    g.fillRect(x, y, 1, 1);
                 }
             }
         }
     }
 
     public void scroll(int dx, int dy) {
-        mScrollX += dx;
-        mScrollY += dy;
+        mScrollX += dx / mScale;
+        mScrollY += dy / mScale;
         repaint();
     }
 
@@ -73,7 +76,21 @@ public class SimulationPanel extends JPanel {
     }
 
     private void setScale(double scale) {
+        if (scale < SCALE_MIN || scale > SCALE_MAX)
+            return;
+
+        int width = getWidth();
+        int height = getHeight();
+
+        int gridWidthOld = (int) Math.ceil(width / mScale) + 1;
+        int gridWidth = (int) Math.ceil(width / scale) + 1;
+
+        int gridHeightOld = (int) Math.ceil(height / mScale) + 1;
+        int gridHeight = (int) Math.ceil(height / scale) + 1;
+
         mScale = scale;
+        mScrollX -= (gridWidth - gridWidthOld) / 2;
+        mScrollY -= (gridHeight - gridHeightOld) / 2;
         repaint();
     }
 
@@ -82,7 +99,7 @@ public class SimulationPanel extends JPanel {
     }
 
     public void zoomOriginal() {
-        setScale(1.0d);
+        setScale(SCALE_DEFAULT);
     }
 
     public void zoomOut() {
