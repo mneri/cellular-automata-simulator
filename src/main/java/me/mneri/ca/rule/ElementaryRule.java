4f9fde31575061ee32e936104aa005fc961aead6
Massimo Neri <hello@mneri.me>
2017-02-08 22:17:07 +0100

    Comments

--- src/main/java/me/mneri/ca/rule/ElementaryRule.java
+++ src/main/java/me/mneri/ca/rule/ElementaryRule.java
@@ -28,6 +28,27 @@ public class ElementaryRule implements Rule {
     public int update(int[] states) {
         int shift = 0;
 
+        /*
+         * Elementary rules are generally represented using the subsequent schema.
+         *
+         *       7          6          5          4          3          2          1          0
+         *  +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+
+         *  |##|##|##| |##|##|  | |##|  |##| |##|  |  | |  |##|##| |  |##|  | |  |  |##| |  |  |  |
+         *  +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+ +--+--+--+
+         *     |  |       |##|       |##|       |  |       |##|       |##|       |##|       |  |
+         *     +--+       +--+       +--+       +--+       +--+       +--+       +--+       +--+
+         *
+         * For each neighbor configuration this schema tells the future state of the cell. Each neighbor configuration
+         * is labelled with a number from 0 to 7, thus the rule can be represented with a number from 0 to 255
+         * (2^7 - 1). The one above is rule 110 (in binary, rule 01101110). Each bit corresponds to a given neighbors'
+         * configuration so, for example, the third bit from the left tells the cell's future state when neighbors'
+         * states are 101.
+         *
+         * Here we convert the neighbors' states into a decimal number and then use that number to shift the rule
+         * number. For example, say we use rule 110 (01101110 in binary) and our neighborhood has the state 101
+         * (that is 5 in decimal). Now, shifting 01101110 to the right of five positions we obtain 00000011. The
+         * rightmost bit is the future state of the cell (and then, 00000011 & 1 = 1).
+         */
         for (int i = 0; i < ARITY; i++) {
             shift <<= 1;
             shift |= states[i];
